081f8ad09abb60a43c5021a9a52fc5b77df392c1
Parisj7 <parisjjfranco@gmail.com>
2022-04-20 21:10:03 -0300

    eliminado el rar viejo

--- FrancoParis/catalog-service-parcial/src/main/java/com/dh/catalogservice/api/service/CatalogService.java
+++
@@ -1,7 +0,0 @@
-package com.dh.catalogservice.api.service;
-
-import com.dh.catalogservice.domain.model.dto.CatalogWS;
-
-public interface CatalogService {
-    CatalogWS getMoviesByGenre(String genre);
-}
